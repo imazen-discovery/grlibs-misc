5b0f3dcaeb24430cacf39a73b37112814a8f1f4b
Chris Reuter <chris@blit.ca>
2013-11-21 16:18:50 -0500

    Added GD blur program.

---
+++ cprogs/blur.c
@@ -0,0 +1,131 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#include <gd.h>
+
+#include "util.h"
+
+enum FType {UNKNOWN, PNG, JPG, GIF, TIFF};
+struct {
+    enum FType id;
+    const char *ext;
+} Types[] = {
+    {PNG,   ".png"},
+    {JPG,   ".jpg"},
+    {JPG,   ".jpeg"},
+    {GIF,   ".gif"},
+    {TIFF,  ".tiff"},
+    
+    {UNKNOWN, NULL}
+};
+
+enum FType
+ftype(const char *filename) {
+    size_t len = strlen(filename);
+    int n;
+
+    check(len > 5, "Invalid filename '%s' (too short).", filename);
+
+    for (n = 0; Types[n].ext; n++) {
+        const char *ext = &filename[len - strlen(Types[n].ext)];
+
+        if (strcasecmp(ext, Types[n].ext) == 0) {
+            return Types[n].id;
+        }/* if */
+    }/* for */
+
+    return UNKNOWN;
+}/* ftype*/
+
+void
+save(gdImagePtr im, const char *template, enum FType type) {
+    char oname[255];
+    FILE *out;
+    const char *ext = NULL;
+    int n;
+
+    for (n = 0, ext = NULL; Types[n].ext; n++) {
+        if (Types[n].id == type) {
+            ext = Types[n].ext;
+            break;
+        }/* if */
+    }/* for */
+    check(!!ext, "Unknown file extension type: %d", type);
+    
+    snprintf(oname, sizeof(oname), "%s.%s", template, ext);
+
+    out = fopen(oname, "wb");
+    check(!!out, "Unable to open '%s' for writing.", oname);
+
+    switch(type) {
+    case PNG:   gdImagePng(im, out);        break;
+    case JPG:   gdImageJpeg(im, out, 100);  break;
+    case GIF:   gdImageGif(im, out);        break;
+    case TIFF:  gdImageTiff(im, out);       break;
+    default:
+        check(0, "invalid type: %d", type);
+    }/* switch*/
+
+    fclose(out);
+}/* save*/
+
+
+gdImagePtr
+load(const char *filename) {
+    gdImagePtr im;
+    FILE *in;
+    enum FType type;
+
+    type = ftype(filename);
+    check(type != UNKNOWN, "Unknown file type for '%s'", filename);
+
+    in = fopen(filename, "r");
+    check(!!in, "Error opening '%s'", filename);
+
+    switch(type) {
+    case PNG:   im = gdImageCreateFromPng(in);      break;
+    case JPG:   im = gdImageCreateFromJpeg(in);     break;
+    case GIF:   im = gdImageCreateFromGif(in);      break;
+    case TIFF:  im = gdImageCreateFromTiff(in);     break;
+
+    default:    im = NULL;
+    }/* switch*/
+        
+    check(!!im, "Error creating input image object.");
+
+    fclose(in);
+
+    return im;
+}/* load*/
+
+
+
+int
+main(int argc, char *argv[]) {
+    char *ifile, *ofile;
+    int radius;
+    gdImagePtr im, result;
+
+    check(argc == 4, "USAGE: blur <input> <radius> <output>");
+
+    ifile = argv[1];
+    radius = atoi(argv[2]);
+    ofile = argv[3];
+
+    check(radius > 0, "Invalid radius.");
+
+    printf("Loading...\n");
+    im = load(ifile);
+
+    printf("Blurring...\n");
+    result = gdImageGaussianBlur2(im, radius);
+    check(!!result, "Gaussian blur failed.");
+
+    printf("Saving...\n");
+    save(result, ofile, ftype(ifile));
+
+    return 0;
+}/* main*/
---
+++ util/blur.rb
@@ -0,0 +1,110 @@
+#!/usr/bin/env ruby
+
+# Simplified version of gd_resize.rb that resizes one image.
+require 'pry'  # DEBUG! REMOVE!
+require 'rubygems'
+
+require 'optparse'
+
+require 'gd2-ffij'
+include GD2
+
+# Add path to local modules
+proc {
+  $LOAD_PATH.unshift(File.dirname(__FILE__))
+}.call()
+
+
+def blur(input, radius, output)
+  im = Image.import(input)
+
+  result = im.blurred(radius)
+  
+  opts = {}
+  case File.extname(output).downcase.to_sym
+  when :jpg, :jpeg
+    opts[:quality] = 100
+    
+    # ... more goes here, maybe ...
+  end
+
+  result.save_alpha = true  # this is off by default
+  moveOldFile(output)
+  result.export(output, opts)
+end
+
+
+# Rename file 'name' to something different and (probably) unique
+def moveOldFile(name)
+  return unless File.file?(name)
+
+  count = 0
+  while true
+    count += 1
+    nfn = "#{name}.#{count}"
+
+    if !File.file?(nfn)
+      File.rename(name, nfn)
+      break
+    end
+  end
+end
+
+
+
+def main
+  truecolor = false
+  mode = 'bicubic'
+  blur = false
+  sharpenPct = 0
+
+  # OptionParser.new do |opts|
+  #   opts.banner =
+  #     "Usage: #{__FILE__} <ifile> <width> [<height>|'-'] <ofile>..."
+
+
+  #   opts.on('--force-truecolor', "Force input images to truecolor.") {
+  #     truecolor = true
+  #   }
+
+  #   opts.on('--interp MODE', '--mode MODE', 
+  #           "Use interpolation mode 'MODE'.") { |im|
+  #     raise "Unknown interpolation mode '#{im}'" unless MODES.has_key?(im)
+  #     mode = im
+  #   }
+
+  #   opts.on('--modes', "List all interpolation modes and exit.") {
+  #     print MODES.keys.sort.join("\n"), "\n"
+  #     exit 0
+  #   }
+
+  #   opts.on('--blur', "Pre-filter image with a gaussian blur.") { |im|
+  #     blur = true
+  #   }
+
+  #   opts.on('--sharpen PERCENT', Float, "Sharpen the resulting image.") { |im|
+  #     sharpenPct = im.round
+  #     if sharpenPct <= 0
+  #       puts "Invalid sharpen percentage: #{sharpenPct}"
+  #       exit 1
+  #     end
+  #   }
+  # end.parse!
+
+  if ARGV.size != 3
+    puts "USAGE: resize <input> <radius> <output>"
+    exit 1
+  end
+
+  r = ARGV[1].to_i
+  if r == 0
+    puts "Invalid radius: #{r}"
+    exit 1
+  end
+
+  blur ARGV[0], r, ARGV[2]
+end
+
+
+main()
+
