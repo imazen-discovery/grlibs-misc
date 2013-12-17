f34ede82bda7adf9aea47c82f64df12b7ade9b5e
Chris Reuter <chris@blit.ca>
2013-12-17 15:02:22 -0500

    Added xbm bug testcase.

---
+++ bugs/gd/109_xbm_segfault/xbmbug.c
@@ -0,0 +1,44 @@
+#/*
+
+# Via the magic of C hackery, this program will compile itself when
+# run as a shell script.
+
+# This is a simple C program which will create a white 300x300 image,
+# export it to "before.png", then scale it to 600x600 and output that
+# as "after.png".  This will reveal that the rigth and bottom lines
+# are now black.
+
+gcc -g -Wall $0 -o ${0%.c} `pkg-config gdlib --cflags --libs`
+exit $?
+
+#*/
+#include <stdio.h>
+
+#include "gd.h"
+
+
+void png(gdImagePtr im, const char *filename)
+{    
+    FILE *fp;
+    fp = fopen(filename, "w");
+    gdImagePng(im, fp);
+    fclose(fp);
+}/* png*/
+
+
+int main(int argc, char **argv)
+{
+    gdImagePtr im;
+    FILE *fp;
+    
+    fp = fopen("img-ref.xbm", "rb");
+    if (!fp) return 1;
+
+    im = gdImageCreateFromTga(fp);
+    printf("image: %lx\n", (unsigned long)im);
+    if (!im) return 1;
+
+    png(im, "after.png");
+
+    return 0;
+}/* main*/
