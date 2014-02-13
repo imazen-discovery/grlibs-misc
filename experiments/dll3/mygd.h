9497b05713c7d91e1098be61b74c1c3b377fd11f
Chris Reuter <chris@blit.removethispart.ca>
2014-02-13 17:40:18 -0500

    Reproduced GD calling convention.

--- experiments/dll3/mygd.h
+++ experiments/dll3/mygd.h
@@ -1,4 +1,8 @@
 
-#define BGD_DECLARE(a) a
+//#define BGD_DECLARE(a) a
+
+#define BGD_EXPORT_DATA_PROT __attribute__ ((dllexport))
+#define BGD_STDCALL __stdcall
+#define BGD_DECLARE(rt) BGD_EXPORT_DATA_PROT rt BGD_STDCALL
 
 BGD_DECLARE(int) gdMajorVersion(void);
